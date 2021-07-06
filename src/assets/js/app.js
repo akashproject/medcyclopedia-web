100e457c7f2dbdece60723cc8fedd5872c337c81
rahulyadav1986 <rahullooksforward@gmail.com>
2021-07-06 19:39:12 +0530

    updated

---
+++ src/assets/js/app.js
@@ -0,0 +1,21 @@
+
+
+$('.mobile_menu').slicknav({
+    label: '',
+    prependTo: '.main_header',
+    closedSymbol: '+',
+    openedSymbol: '-',
+    allowParentLinks:"true",
+});
+
+
+
+
+
+
+
+
+
+
+
+
